888ea5a955013167088239492d6dbc645f6cd122
siriuzz <1106901@est.intec.edu.do>
2023-02-14 16:43:36 -0400

    tictactoe

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,20 +1,104 @@
-// TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// TicTacToeEDAII.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+using namespace std;
+
+char tablero[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+bool turno = true;
+
+void DesplegarTablero() {
+
+    for (int i = 0; i < 3; i++) {
+        cout << "  ";
+        for (int j = 0; j < 3; j++) {
+            cout << tablero[i][j];
+            if (j < 2) cout << "  |  ";
+        }
+        cout << endl;
+        if (i < 2) {
+            cout << "-----+-----+-----" << endl;
+        }
+    }
+}
+
+bool JuegoBloqueado() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tablero[i][j] != '0' && tablero[i][j] != 'X') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool GameOver(char playerChar) {
+    for (int i = 0; i < 3; i++) {
+        if (tablero[i][0] == playerChar && tablero[i][1] == playerChar && tablero[i][2] == playerChar) {
+            return true;
+        }
+        else if (tablero[0][i] == playerChar && tablero[1][i] == playerChar && tablero[2][i] == playerChar) {
+            return true;
+        }
+    }
+
+    if (tablero[0][0] == playerChar && tablero[1][1] == playerChar && tablero[2][2] == playerChar) {
+        return true;
+    }
+    else if (tablero[0][2] == playerChar && tablero[1][1] == playerChar && tablero[2][0] == playerChar) {
+        return true;
+    }
+
+    return false;
+}
+
+void Jugada(char playerChar, int position) {
+    int counter = 1;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (counter == position) {
+                tablero[i][j] = playerChar;
+                return;
+
+            }
+            counter++;
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char playerChar = 'O';
+    int position;
+    cout << "Bienvenido al juego Tic Tac Toe!" << endl;
+
+    while (true) {
+        DesplegarTablero();
+
+        cin >> position;
+
+        Jugada(playerChar, position);
+
+
+        if (GameOver(playerChar)) {
+            DesplegarTablero();
+            cout << "El jugador de las " << playerChar << " es el ganador!" << endl;
+            break;
+        }
+
+        if (JuegoBloqueado()) {
+            DesplegarTablero();
+
+            cout << "El juego es un empate (no mas jugadas validas)" << endl;
+            break;
+        }
+
+        if (playerChar == 'O') playerChar = 'X';
+        else if (playerChar == 'X') playerChar = 'O';
+
+
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
